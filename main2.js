78ba4e3164e6a8f0df4ad798a435fb17cc205cdf
juangruoficial <cruz9606@gmail.com>
2023-07-11 18:08:34 -0500

    adding the toogle darkMode botton function

--- main2.js
+++ main2.js
@@ -7,11 +7,12 @@ async function getProductsApi() {
     window.localStorage.setItem("products", JSON.stringify(res));
     return res;
   } catch (error) {
-    console.log("Error while fetching products:");
+    console.log("Error while fetching products:", error);
   }
 }
 
 function printProducts(dataBase) {
+  const productsContainer = document.querySelector(".products");
   let html = "";
 
   dataBase.products.forEach(function (product) {
@@ -20,40 +21,34 @@ function printProducts(dataBase) {
       ? `<i class='bx bx-plus' id='${id}'></i>`
       : `<span class='sold__out'>Sold Out</span>`;
     const buttonMinus = `<i class='bx bx-minus' id='${id}'></i>`;
-
     const buttonTrash = `<i class='bx bxs-trash' id='${id}'></i>`;
 
-    html += `
-                      <div class="product ${category}" >
-                             <div class="product__img">
-                                    <img src="${image}" alt="${name}" />
-                             </div>
- 
-                             <div class="product__info">
-                             <h4>${name} |<span><b>Stock</b>: ${quantity}</span></h4>
-                             <h5>${buttonMinus}
-                                    $${price}.0
-                                    ${buttonAdd}
-                                    ${buttonTrash}
-                                    <span class='amount' id='${id}'></span>
-                             </h5>
-                             </div>
-                             
-                      </div>
-               `;
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
+    productElement.classList.add(category);
+
+    const productImg = document.createElement("div");
+    productImg.classList.add("product__img");
+    const img = document.createElement("img");
+    img.src = image;
+    img.alt = name;
+    productImg.appendChild(img);
+    productElement.appendChild(productImg);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product__info");
+    const productName = document.createElement("h4");
+    productName.innerHTML = `${name} |<span><b>Stock</b>: ${quantity}</span>`;
+    const productPrice = document.createElement("h5");
+    productPrice.innerHTML = `${buttonMinus}$${price}.0${buttonAdd}${buttonTrash}<span class='amount' id='${id}'></span>`;
+    productInfo.appendChild(productName);
+    productInfo.appendChild(productPrice);
+    productElement.appendChild(productInfo);
+
+    html += productElement.outerHTML;
   });
 
-  document.querySelector(".products").innerHTML = html;
-  //printAmountOnProducts(dataBase);
-}
-
-function showHideCart() {
-  const carIconHTML = document.querySelector(".bx-cart-alt");
-  const cartHTML = document.querySelector(".cart");
-
-  carIconHTML.addEventListener("click", function () {
-    cartHTML.classList.toggle("cart__show");
-  });
+  productsContainer.innerHTML = html;
 }
 
 function toggleDarkMode() {
@@ -78,21 +73,43 @@ function checkDarkMode() {
   }
 }
 
+function showHideCart() {
+  const carIconHTML = document.querySelector(".bx-cart-alt");
+  const cartHTML = document.querySelector(".cart");
+
+  const toggleCart = () => {
+    cartHTML.classList.toggle("cart__show");
+  };
+
+  carIconHTML.addEventListener("click", toggleCart);
+}
+
+/* function darkMode() {
+  const moonIconHTML = document.querySelector(".bxs-moon");
+  const dark__showHTML = document.querySelector(".main__container");
+
+  moonIconHTML.addEventListener("click", function (e) {
+    console.log();
+
+    dark__showHTML.classlist.toggle("dark__mode");
+  });
+} */
+
 function addToCartFromProducts(dataBase) {
   const productsHTML = document.querySelector(".products");
 
-  productsHTML.addEventListener("click", function (element) {
-    if (element.target.classList.contains("bx-plus")) {
-      //console.log(element.target.id);
-      const id = Number(element.target.id);
+  const handleAddToCart = (event) => {
+    if (event.target.classList.contains("bx-plus")) {
+      const id = Number(event.target.id);
 
-      const findProduct = dataBase.products.find(function (element) {
-        return element.id === id;
-      });
+      const findProduct = dataBase.products.find(
+        (product) => product.id === id
+      );
 
       if (dataBase.cart[findProduct.id]) {
-        if (findProduct.quantity === dataBase.cart[findProduct.id].amount)
-          return alert("Not enough in Stock quantity");
+        if (findProduct.quantity === dataBase.cart[findProduct.id].amount) {
+          return alert("Not enough quantity in stock");
+        }
         dataBase.cart[findProduct.id].amount++;
       } else {
         dataBase.cart[findProduct.id] = { ...findProduct, amount: 1 };
@@ -104,17 +121,18 @@ function addToCartFromProducts(dataBase) {
       printInfoTotal(dataBase);
       printAmountProducts(dataBase);
     }
-  });
+  };
+
+  productsHTML.addEventListener("click", handleAddToCart);
 }
 
 function minusToCartFromProducts(dataBase) {
   const productsHTML = document.querySelector(".products");
 
-  productsHTML.addEventListener("click", function (element) {
-    if (element.target.classList.contains("bx-minus")) {
-      const id = Number(element.target.id);
+  const handleMinusFromCart = (event) => {
+    if (event.target.classList.contains("bx-minus")) {
+      const id = Number(event.target.id);
 
-      // hacer funcion
       if (dataBase.cart[id].amount === 1) {
         const res = confirm("Are you sure you want to delete this product?");
         if (!res) return;
@@ -122,7 +140,6 @@ function minusToCartFromProducts(dataBase) {
       } else {
         dataBase.cart[id].amount--;
       }
-      //termina la funcion
 
       window.localStorage.setItem("cart", JSON.stringify(dataBase.cart));
 
@@ -130,56 +147,62 @@ function minusToCartFromProducts(dataBase) {
       printInfoTotal(dataBase);
       printAmountProducts(dataBase);
     }
-  });
+  };
+
+  productsHTML.addEventListener("click", handleMinusFromCart);
 }
 
 function printProductsToCart(dataBase) {
   const cartProductsHTML = document.querySelector(".cart__products");
   let html = "";
+
   for (const product in dataBase.cart) {
     const { amount, quantity, price, name, image, id } = dataBase.cart[product];
-    html += `<div class="cart__product">
-               <div class="cart__product__img">
-                    <img src='${image}' alt='${name}' />
+
+    html += `
+           <div class="cart__product">
+             <div class="cart__product__img">
+               <img src='${image}' alt='${name}' />
+             </div>
+             <div class='cart__product__body'>
+               <h4> ${name} | $${price}.0 </h4>
+               <p>Stock: ${quantity}</p>
+               <div class='cart__product__opt' id='${id}'>
+                 <i class='bx bx-minus'></i>
+                 <span>${amount}</span>
+                 <i class='bx bx-plus'></i>
+                 <i class='bx bxs-trash'></i>
                </div>
-               <div class='cart__product__body'>
-                    <h4> ${name} | $${price}.0 </h4>
-                    
-                    <p>Stock: ${quantity}</p>
-                    <div class='cart__product__opt' id='${id}'>
-                    <i class='bx bx-minus'></i>
-                    <span>${amount}</span>
-                    <i class='bx bx-plus' ></i>
-                    <i class='bx bxs-trash' ></i>
-                    </div>
-               </div>
-          </div>`;
+             </div>
+           </div>
+         `;
   }
+
   cartProductsHTML.innerHTML = html;
 }
 
 function handlePodructsFromCart(dataBase) {
-  const cart__productsHTML = document.querySelector(".cart__products");
+  const cartProductsHTML = document.querySelector(".cart__products");
 
-  cart__productsHTML.addEventListener("click", function (element) {
-    if (element.target.classList.contains("bx-plus")) {
-      const id = Number(element.target.parentElement.id);
+  const handleCartProductClick = (event) => {
+    const targetClassList = event.target.classList;
 
-      //hacer en funcion
-      const findProduct = dataBase.products.find(function (element) {
-        return element.id === id;
-      });
-      if (findProduct.quantity === dataBase.cart[findProduct.id].amount)
-        return alert("Not enough in Stock quantity");
+    if (targetClassList.contains("bx-plus")) {
+      const id = Number(event.target.parentElement.id);
+      const findProduct = dataBase.products.find(
+        (product) => product.id === id
+      );
 
-      //termina aqui
+      if (findProduct.quantity === dataBase.cart[findProduct.id].amount) {
+        return alert("Not enough quantity in stock");
+      }
+
       dataBase.cart[id].amount++;
     }
 
-    if (element.target.classList.contains("bx-minus")) {
-      const id = Number(element.target.parentElement.id);
+    if (targetClassList.contains("bx-minus")) {
+      const id = Number(event.target.parentElement.id);
 
-      // hacer funcion
       if (dataBase.cart[id].amount === 1) {
         const res = confirm("Are you sure you want to delete this product?");
         if (!res) return;
@@ -187,39 +210,33 @@ function handlePodructsFromCart(dataBase) {
       } else {
         dataBase.cart[id].amount--;
       }
-      //termina la funcion
     }
 
-    if (element.target.classList.contains("bxs-trash")) {
-      const id = Number(element.target.parentElement.id);
-      if (dataBase.cart[id].amount) {
-        const res = confirm("Are you sure you want to delete this product?");
-        if (!res) return;
-        delete dataBase.cart[id];
-      } else {
-        dataBase.cart[id].amount--;
-      }
+    if (targetClassList.contains("bxs-trash")) {
+      const id = Number(event.target.parentElement.id);
+      const res = confirm("Are you sure you want to delete this product?");
+      if (!res) return;
+      delete dataBase.cart[id];
     }
 
     window.localStorage.setItem("cart", JSON.stringify(dataBase.cart));
     printProductsToCart(dataBase);
     printInfoTotal(dataBase);
     printAmountProducts(dataBase);
-  });
+  };
+
+  cartProductsHTML.addEventListener("click", handleCartProductClick);
 }
 
 function printButtonBuy(dataBase) {
-  let html = "";
-  html = `
-          <div class="cart__total__info">
-               <span class='info__amount'></span>
-               <span class='info__total'></span>
-          </div>
+  const html = `
+         <div class="cart__total__info">
+           <span class='info__amount'></span>
+           <span class='info__total'></span>
+         </div>
+         <button type="button" class="btn__buy">Buy</button>
+       `;
 
-          <button type="button" class="btn__buy">Buy
-          </button>
-
-             `;
   document.querySelector(".cart__total").innerHTML = html;
   printInfoTotal(dataBase);
 }
@@ -237,7 +254,7 @@ function printInfoTotal(dataBase) {
     amountProducts += amount;
   }
 
-  infoTotalHTML.textContent = `$${totalPrice}.00`;
+  infoTotalHTML.textContent = `$${totalPrice.toFixed(2)}`;
   infoAmountHTML.textContent = `${amountProducts} Units`;
 }
 
@@ -245,24 +262,23 @@ function handletoBuy(dataBase) {
   const btnBuyHTML = document.querySelector(".btn__buy");
 
   btnBuyHTML.addEventListener("click", function (element) {
-    if (!Object.values(dataBase.cart).length)
-      return alert("Please add at least one");
-    const res = confirm("Are you sure you want to buy this items?");
+    if (Object.values(dataBase.cart).length === 0)
+      return alert("Please add at least one item to the cart.");
+
+    const res = confirm("Are you sure you want to buy these items?");
     if (!res) return;
 
-    const currentProducts = [];
-
-    for (const product of dataBase.products) {
+    const currentProducts = dataBase.products.map((product) => {
       const productCart = dataBase.cart[product.id];
-      if (product.id === productCart?.id) {
-        currentProducts.push({
+      if (productCart) {
+        return {
           ...product,
           quantity: product.quantity - productCart.amount,
-        });
+        };
       } else {
-        currentProducts.push(product);
+        return product;
       }
-    }
+    });
 
     dataBase.products = currentProducts;
     dataBase.cart = {};
@@ -273,13 +289,14 @@ function handletoBuy(dataBase) {
     printProductsToCart(dataBase);
     printAmountProducts(dataBase);
     printProducts(dataBase);
-    location.href = location.href;
+    location.reload();
   });
 }
 
 function printAmountProducts(dataBase) {
   const amountProductsHTML = document.querySelector(".cart__amount__product");
   let amount = 0;
+
   for (const product in dataBase.cart) {
     amount += dataBase.cart[product].amount;
   }
@@ -293,38 +310,16 @@ function trashtoCartFromProduct(dataBase) {
   productsHTML.addEventListener("click", function (element) {
     if (element.target.classList.contains("bxs-trash")) {
       const id = Number(element.target.id);
-
-      for (const product in dataBase.cart) {
-        const { amount } = dataBase.cart[product];
-
-        if (amount) {
-          const res = confirm("Are you sure you want to delete this product?");
-          if (!res) return;
-          delete dataBase.cart[id];
-          console.log(dataBase.cart[id]);
-        }
-      }
+      const res = confirm("Are you sure you want to delete this product?");
+      if (!res) return;
+      delete dataBase.cart[id];
     }
-    //termina la funcion
 
     window.localStorage.setItem("cart", JSON.stringify(dataBase.cart));
     printProductsToCart(dataBase);
     printInfoTotal(dataBase);
     printAmountProducts(dataBase);
   });
-
-  // hacer funcion
-
-  //termina la funcion
-
-  /*  window.localStorage.setItem("cart", JSON.stringify(dataBase.cart));
-              printProducts(dataBase);
-              printProductsToCart(dataBase);
-              printAmountProducts(dataBase);
-              printInfoTotal(dataBase);
-              printAmountProducts(dataBase);
-              addToCartFromProducts(dataBase);
-            }); */
 }
 
 function handleFilters(dataBase) {
@@ -347,14 +342,10 @@ async function main() {
     cart: JSON.parse(window.localStorage.getItem("cart")) || {},
   };
 
-  //saveToLocalStorage("products", dataBase.products);
-  //getfromLocalStorage("products", dataBase.products);
+  printProducts(dataBase);
   toggleDarkMode();
   checkDarkMode();
-  printProducts(dataBase);
-
   showHideCart();
-
   addToCartFromProducts(dataBase);
   minusToCartFromProducts(dataBase);
   printProductsToCart(dataBase);
