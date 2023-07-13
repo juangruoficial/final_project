12c278a1a6d251b4a12463ae8f9aa9adfcd34030
juangruoficial <cruz9606@gmail.com>
2023-07-13 18:27:10 -0500

    preloasd

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,17 @@ async function getProductsApi() {
   }
 }
 
+function loader() {
+  setTimeout(function () {
+    document
+      .querySelector(".content__loader")
+      .classList.add("content__loader__hidden");
+  }, 1000);
+}
+window.addEventListener("load", function () {
+  loader();
+});
+
 function printProducts(dataBase) {
   let html = "";
 
